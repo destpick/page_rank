cfe0f58deeae564a625112cc459c3930a4f35add
Desta <desta.pickering@wsu.edu>
2019-12-08 23:23:24 -0800

    hello

--- Graph.h
+++ Graph.h
@@ -7,19 +7,20 @@
 #include <stdlib.h>
 #include <cstdlib>
 #include <omp.h>
+#include <unordered_map>
 #include "Page.h"
 
 class Graph
 {
 
 private:
-	Page** complete_list;
+	std::unordered_map<int, Page*> *complete_list;
 	int num_of_pages;
 
 public:
 	Graph()
 	{
-		this->complete_list = nullptr;
+		this->complete_list = new std::unordered_map<int, Page*>();
 		this->num_of_pages = 0;
 	}
 
@@ -33,13 +34,18 @@ public:
 		return this->num_of_pages;
 	}
 
-	Page* get_complete_list(int index)
+	Page * get_page_at_position(int index)
 	{
-		return this->complete_list[index];
+		return this->complete_list->at(index);
 	}
-	Page** return_list()
+
+	Page *return_complete_list(int temp)
 	{
-		return this->complete_list;
+		return this->complete_list->at(temp);
+	}
+	std::unordered_map<int, Page *> get_list()
+	{
+		return *this->complete_list;
 	}
 
 	void increment_page()
@@ -55,276 +61,224 @@ public:
 	*/
 	int find_existing_page(int id)
 	{
-		int placement = 0;
-		// If network is empty want to signal that we need to add this new id
-		if (this->complete_list == nullptr)
+		if (id >= this->num_of_pages)
 		{
-			std::cout << "Empty network." << std::endl;
+			return 0;
 		}
 
 		else
 		{
-			for (int index = 0; index < this->num_of_pages; index++)
-			{
-				if (id == this->complete_list[index]->get_page_id())
-				{
-					placement = 1;
-				}
-			}
-
-		}
-		return placement;
-	}
-	void remove_duplicate()
-	{
-		for (int j = 0; j < num_of_pages; j++)
-		{
-			for (int i = 1; i < num_of_pages-1; i++)
-			{
-				int temp_i = this->complete_list[i]->get_page_id();
-				int temp_j = this->complete_list[j]->get_page_id();
-				if (temp_i == temp_j)
-				{
-					Page* temp = this->complete_list[i];
-					for (int k = i; k < num_of_pages; k++)
-					{
-						this->complete_list[k] = this->complete_list[k + 1];
-					}
-				}
-			}
-		}
-	}
-		// Returns the placement of node
-		int position_of_node(int id)
-		{
-
-			for (int index = 0; index < this->num_of_pages; index++)
-			{
-				if (id == this->complete_list[index]->get_page_id())
-				{
-					return index;
-				}
-			}
-		}
-
-		/*
-				Adding a page to the existing network
-				Return:
-						true if successfully added
-						false if some error occurred
-		*/
-		void insert_page(int coming_from, int going_to)
-		{
-
-			if (this->complete_list == nullptr)
-			{
-				this->complete_list = new Page * [1];
-			}
-
-			if ((this->find_existing_page(coming_from)) == 1)
-			{
-				this->complete_list[this->position_of_node(coming_from)]->set_outgoing_link(going_to);
-			}
-
-			if ((this->find_existing_page(coming_from)) == 0)
-			{
-				Page* new_page = new Page(coming_from);
-				this->complete_list = (Page**)realloc(complete_list, (this->num_of_pages + 1) * sizeof(Page*));
-				this->complete_list[this->num_of_pages] = new_page;
-				this->complete_list[this->num_of_pages]->set_outgoing_link(going_to);
-				this->num_of_pages++;
-			}
-
-			// Adds child page to the network if it doesnt already exist
-			if ((this->find_existing_page(going_to)) == 0)
-			{
-				Page* new_page = new Page(going_to);
-				this->complete_list = (Page**)realloc(complete_list, (this->num_of_pages + 1) * sizeof(Page*));
-				this->complete_list[this->num_of_pages] = new_page;
-				this->num_of_pages++;
-			}
-
-		}
-	};
-	// Parses file and creates the appropriate represented network
-	void create_network(Graph*& network, std::ifstream& input_file)
-	{
-		int parent_id = 0;
-		int child_id = 0;
-
-		std::string current_line;
-
-		while (std::getline(input_file, current_line))
-		{
-			// Ignore all lines that are comments
-			if (current_line[0] == '#')
-			{
-				continue;
-			}
-
-			// creates nodes
-			else
-			{
-				std::stringstream pair_nodes(current_line);
-				pair_nodes >> parent_id >> child_id;
-				network->insert_page(parent_id, child_id);
-			}
+			return 1;
 		}
 	}
 
-	void print_id(Graph*& network)
+
+	/*
+	// Returns the placement of node
+	int position_of_node(int id)
 	{
-		for (int j = 0; j < network->get_num_of_pages(); j++)
+		for (int index = 0; index < this->num_of_pages; index++)
 		{
-			for (int i = j+1; i < network->get_num_of_pages(); i++)
+			if (id == this->complete_list[index])
 			{
-				if (network->get_complete_list(j)->get_page_id() == network->get_complete_list(i)->get_page_id())
-				{
-					std::cout << network->get_complete_list(j)->get_page_id() << " " << network->get_complete_list(i)->get_page_id() << std::endl;
-					std::cout << "duplicates" << std::endl;
-				}
+				return index;
 			}
 		}
-		std::cout << "no duplicates" << std::endl;
 	}
-
-	bool coin_flip(double dampening_ratio)
+	*/
+	/*
+			Adding a page to the existing network
+			Return:
+					true if successfully added
+					false if some error occurred
+	*/
+	void insert_page(int coming_from, int going_to)
 	{
-		double random_chance = 0.0;
-		srand((int)time(NULL) ^ omp_get_thread_num());
-
-		random_chance = ((double)rand()) / RAND_MAX;
-
-		if (dampening_ratio > random_chance)
+		 if (this->complete_list->count(coming_from) == 0)
 		{
-			return false;
+			Page* new_node = new Page(coming_from);
+			new_node->set_outgoing_link(going_to);
+			this->complete_list->insert({ coming_from,new_node });
+			this->increment_page();
+
 		}
-		return true;
-	}
-
-	int random_number(int max_length)
-	{
-		srand((int)time(NULL) ^ omp_get_thread_num());
-
-		return (rand() % max_length);
-
-	}
-	void page_rank(Graph*& network, int walk_count, double dampening_ratio)
-	{
-		int start_node = 0;
-		int current_step = 0;
-		#pragma omp parallel for schedule(static)shared(network, walk_count, dampening_ratio) private(start_node, current_step)
-		for (start_node = 0; start_node < network->get_num_of_pages(); start_node++)
+		 if (this->complete_list->count(going_to) == 0)
+		 {
+			 Page* new_node = new Page(going_to);
+			 this->complete_list->insert({ going_to, new_node });
+			 this->increment_page();
+		 }
+		else
 		{
-			int current_node = start_node;
-			for (current_step = 0; current_step < walk_count; current_step++)
+			this->complete_list->at(coming_from)->set_outgoing_link(going_to);
+		}
+	}
+};
+
+// Parses file and creates the appropriate represented network
+void create_network(Graph*& network, std::ifstream& input_file)
+{
+	int parent_id = 0;
+	int child_id = 0;
+
+	std::string current_line;
+	int counter = 0;
+	while (std::getline(input_file, current_line))
+	{
+		// Ignore all lines that are comments
+		if (current_line[0] == '#')
+		{
+			continue;
+		}
+
+		// creates nodes
+		else
+		{
+			std::stringstream pair_nodes(current_line);
+			pair_nodes >> parent_id >> child_id;
+			network->insert_page(parent_id, child_id);
+			
+		}
+	}
+}
+
+bool coin_flip(double dampening_ratio)
+{
+	double random_chance = 0.0;
+	srand((int)time(NULL)); //^ omp_get_thread_num());
+
+	random_chance = ((double)rand()) / RAND_MAX;
+
+	std::cout << random_chance << std::endl;
+	if (dampening_ratio > random_chance)
+	{
+		return false;
+	}
+	return true;
+}
+
+int random_number(int max_length)
+{
+	srand((int)time(NULL) ^ omp_get_thread_num());
+
+	return (rand() % max_length);
+
+}
+void page_rank(Graph*& network, int walk_count, double dampening_ratio)
+{
+	int start_node = 0;
+	int current_step = 0;
+	#pragma omp parallel for schedule(static)shared(network, walk_count, dampening_ratio) private(start_node, current_step)
+	for (start_node = 0; start_node < network->get_num_of_pages(); start_node++)
+	{
+		int current_node = start_node;
+		for (current_step = 0; current_step < walk_count; current_step++)
+		{
+
+			// coin flip == false then must jump to a random neighbors page
+			if (!(coin_flip(dampening_ratio)))
 			{
-				
-				// coin flip == false then must jump to a random neighbors page
-				if (!(coin_flip(dampening_ratio)))
-				{
-					// If no outgoing ports randomly select the next page
-					if (network->get_complete_list(current_node)->get_ports() == 0)
-					{
-
-						int index = random_number(network->get_num_of_pages());
-						current_node = index;
-					}
-					else
-					{
-						int index = random_number(network->get_complete_list(current_node)->get_ports());
-						Page* temp = network->get_complete_list(current_node);
-						int temp_id = temp->get_outgoing_link_id(index);
-
-						current_node = network->position_of_node(temp_id);
-					}
-				}
-
-				// coin flip == true then must jump to a random page
-				else
+				auto size_of_current_outgoing_link = network->return_complete_list(current_node)->get_outlink_size();
+				// If no outgoing ports randomly select the next page
+				if (size_of_current_outgoing_link == 0)
 				{
 					int index = random_number(network->get_num_of_pages());
 					current_node = index;
 				}
-				network->get_complete_list(current_node)->increase_time_visited();
+				else
+				{
+					int index = random_number(size_of_current_outgoing_link);
+					Page* temp = network->return_complete_list(current_node);
+					int temp_id = temp->get_outgoing_link_id(index);
+					current_node = temp_id;
+				}
 			}
+
+			// coin flip == true then must jump to a random page
+			else
+			{
+				int index = random_number(network->get_num_of_pages());
+				current_node = index;
+			}
+			network->return_complete_list(current_node)->increase_time_visited();
 		}
 	}
+}
 
-	void display_network(Page** cur_list, int itt)
+void display_network(Page** cur_list, int itt)
+{
+	for (int i = 0; i < itt; i++)
 	{
-		for (int i = 0; i < itt; i++)
-		{
-			std::cout << "--------------" << std::endl;
-			std::cout << "page id" << cur_list[i]->get_page_id() << std::endl;
-			std::cout << "page_rank" << cur_list[i]->get_page_rank() << std::endl;
-			std::cout << "--------------" << std::endl;
-		}
+		std::cout << "--------------" << std::endl;
+		std::cout << "page id" << cur_list[i]->get_page_id() << std::endl;
+		std::cout << "page_rank" << cur_list[i]->get_page_rank() << std::endl;
+		std::cout << "--------------" << std::endl;
 	}
+}
 
-	void calculate_page_rank(Graph*& network, int total_visits)
+void calculate_page_rank(Graph*& network, int total_visits)
+{
+	for (int index = 0; index < network->get_num_of_pages(); index++)
 	{
-		for (int index = 0; index < network->get_num_of_pages(); index++)
-		{
-			network->get_complete_list(index)->set_page_rank(total_visits);
-		}
+		network->return_complete_list(index)->set_page_rank(total_visits);
 	}
-	void find_first_five_max(Graph*& network, Page** first_five, int total_visits)
+}
+void find_first_five_max(Graph*& network, Page** first_five, int total_visits)
+{
+	calculate_page_rank(network, total_visits);
+	// Sorts the network so the greatest are stored in the top_five
+
+	for (int index = 0; index < network->get_num_of_pages(); index++)
 	{
-		calculate_page_rank(network, total_visits);
-		// Sorts the network so the greatest are stored in the top_five
-
-		for (int index = 0; index < network->get_num_of_pages(); index++)
+		auto current_page = network->return_complete_list(index);
+		if (current_page->get_page_rank() >= (*first_five[0]).get_page_rank())
 		{
-			auto current_page = network->get_complete_list(index);
-			if (current_page->get_page_rank() > (*first_five[0]).get_page_rank())
+			for (int guide = 0; guide < 5; guide++)
 			{
-				for (int guide = 0; guide < 5; guide++)
-				{
-					first_five[guide + 1] = first_five[guide];
-				}
-
-				first_five[0] = current_page;
+				first_five[guide + 1] = first_five[guide];
 			}
 
-			else if (current_page->get_page_rank() > (*first_five[1]).get_page_rank())
-			{
-				for (int guide = 1; guide < 5; guide++)
-				{
-					first_five[guide + 1] = first_five[guide];
-				}
-
-				first_five[1] = current_page;
-			}
-
-			else if (current_page->get_page_rank() > (*first_five[2]).get_page_rank())
-			{
-				for (int guide = 2; guide < 5; guide++)
-				{
-					first_five[guide + 1] = first_five[guide];
-				}
-
-				first_five[2] = current_page;
-			}
-
-			else if (current_page->get_page_rank() > (*first_five[3]).get_page_rank())
-			{
-				for (int guide = 3; guide < 5; guide++)
-				{
-					first_five[guide + 1] = first_five[guide];
-				}
-
-				first_five[3] = current_page;
-			}
-
-			else if (current_page->get_page_rank() > (*first_five[4]).get_page_rank())
-			{
-				first_five[4] = current_page;
-			}
-
-
+			first_five[0] = current_page;
 		}
+
+		else if (current_page->get_page_rank() >= (*first_five[1]).get_page_rank())
+		{
+			for (int guide = 1; guide < 5; guide++)
+			{
+				first_five[guide + 1] = first_five[guide];
+			}
+
+			first_five[1] = current_page;
+		}
+
+		else if (current_page->get_page_rank() >= (*first_five[2]).get_page_rank())
+		{
+			for (int guide = 2; guide < 5; guide++)
+			{
+				first_five[guide + 1] = first_five[guide];
+			}
+
+			first_five[2] = current_page;
+		}
+
+		else if (current_page->get_page_rank() >= (*first_five[3]).get_page_rank())
+		{
+			for (int guide = 3; guide < 5; guide++)
+			{
+				first_five[guide + 1] = first_five[guide];
+			}
+
+			first_five[3] = current_page;
+		}
+
+		else if (current_page->get_page_rank() >= (*first_five[4]).get_page_rank())
+		{
+			first_five[4] = current_page;
+		}
+
+
 	}
+}
 
 
 #endif
--- Page.h
+++ Page.h
@@ -3,148 +3,96 @@
 
 #include <string>
 #include <iostream>
-
+#include <vector>
 ///////////////////ADD GETTER AND SETTER FOR PORTS
 class Page
 {
-	private:
-		int * outgoing_links;
-		int ports;
-		int page_id;
-		int times_visited;
-		long double page_rank;
-	
-	public:
-		////////////////////////////////////////// CONSTRUCTORS //////////////////////
-		//Empty
-		Page()
-		{
-			this->outgoing_links = 0;
-			this->ports = 0;
-			this->page_id = -1;
-			this->times_visited = 0;
-			this->page_rank = 0;
-		}
+private:
+	std::vector<int> *outgoing_links;
+	int page_id;
+	int times_visited;
+	long double page_rank;
+
+public:
+	////////////////////////////////////////// CONSTRUCTORS //////////////////////
+	//Empty
+	Page()
+	{
+		this->outgoing_links = new std::vector<int>();
+		this->page_id = -1;
+		this->times_visited = 0;
+		this->page_rank = 0;
+	}
+
+	// Adding a page that has an id already
+	Page(int id)
+	{
+		this->outgoing_links = new std::vector<int>();
+		this->page_id = id;
+		this->times_visited = 0;
+		this->page_rank = 0;
+	}
+
+	////////////////////////// APPROPRIATE GETTERS AND SETTERS ///////////////////////
+
+	int get_outlink_size()
+	{
+		return this->outgoing_links->size();
+	}
+
+	int get_page_id()
+	{
+		return this->page_id;
+	}
+
+	void set_page_id(int id)
+	{
+		this->page_id = id;
+	}
+
+	void set_page_rank(int total_visits)
+	{
+		this->page_rank = (long double)this->times_visited / total_visits;
+	}
+
+	long double get_page_rank()
+	{
+		return this->page_rank;
+	}
+	int get_time_visited()
+	{
+		return this->times_visited;
+	}
+
+	void increase_time_visited()
+	{
+		this->times_visited++;
+	}
+
+	void set_time_visited(int num)
+	{
+		this->times_visited = num;
+	}
+
+	int get_outgoing_link_id(int index)
+	{
+		return this->outgoing_links->at(index);
+	}
+
+	void set_outgoing_link(int id)
+	{
 		
-		// Adding a page that has an id already
-		Page(int id)
+		if (std::find(this->outgoing_links->begin(), this->outgoing_links->end(), id) == this->outgoing_links->end())
 		{
-			this->outgoing_links = 0;
-			this->ports = 0;
-			this->page_id = id;
-			this->times_visited = 0;
-			this->page_rank = 0;
+			this->outgoing_links->push_back(id);
 		}
-
-		////////////////////////// APPROPRIATE GETTERS AND SETTERS ///////////////////////
-
-		int get_page_id()
-		{
-			return this->page_id;
-		}
-
-		void set_page_id(int id)
-		{
-			this->page_id = id;
-		}
-
-		void set_page_rank(int total_visits)
-		{
-			this->page_rank = (long double)this->times_visited / total_visits;
-		}
-
-		long double get_page_rank()
-		{
-			return this->page_rank;
-		}
-		int get_time_visited()
-		{
-			return this->times_visited;
-		}
-
-		void increase_time_visited()
-		{
-			this->times_visited++;
-		}
-
-		void set_time_visited(int num)
-		{
-			this->times_visited = num;
-		}
-
-		void increment_port()
-		{
-			this->ports++;
-		}
-
-		int get_ports()
-		{
-			return this->ports;
-		}
-
-		int get_outgoing_link_id(int index)
-		{
-			return this->outgoing_links[index];
-		}
-
-		/*
-			Finding the link at a specific position
-			Return:
-				index if exisit
-				-1 if index does not exist
-		*/
-		int get_outgoing_link_position(int page)
-		{
-			if (this->outgoing_links[page])
-			{
-				return this->outgoing_links[page];
-			}
-
-			else
-			{
-				return -1;
-			}
-		}
-		int link_already_exist(int id)
-		{
-			for (int i = 0; i < this->ports; i++)
-			{
-				if (id == this->outgoing_links[i])
-				{
-					return 1;
-				}
-			}
-			return 0;
-		}
-		void set_outgoing_link(int id)
-		{
-			// If first create the area and then fill in necessary info
-			if (this->outgoing_links == nullptr)
-			{
-				this->outgoing_links = new int[1];
-				this->outgoing_links[this->ports] = id;
-				this->ports++;
-			}
-
-			/*
-				If adding to an exisiting list: 
-					Resize the array
-					Add the id 
-					Store the new number of ports to the page
-			*/
-			if(link_already_exist(id) == 0)
-			{
-				this->outgoing_links = (int*)realloc(this->outgoing_links, (this->ports + 1) * sizeof(int));
-				this->outgoing_links[this->ports] = id;
-				this->ports++;
-			}
-		}
+	}
 };
 
 void create_empty_size_five_array(Page** temp)
 {
 	Page* new_refrance = new Page(-1);
-	
+
 	for (int i = 0; i < 5; i++)
 	{
 		temp[i] = new_refrance;
